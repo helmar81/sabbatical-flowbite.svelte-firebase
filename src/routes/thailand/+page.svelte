7ce37e9e63ce0331f09ffbd74444a0d0f8e495a2
helmar81 <helmar81@gmail.com>
2023-04-30 19:59:21 +0200

    next commit

--- src/routes/thailand/+page.svelte
+++ src/routes/thailand/+page.svelte
@@ -94,8 +94,20 @@
     <div>
         <img class="h-auto max-w-full rounded-lg" src="a19.jpg" alt="">
     </div>
+    <Button class="flex items-stretch hover:items-center self-center" pill size="xl" color="alternative">
+        <a href="/india"
+        >N E X T</a
+        > <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 ml-2"><path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" /></svg>
+      </Button>
+
 </div>
 
 
 
+    
+
+
+
+      <!-- svelte-ignore a11y-missing-content -->
+   
 
